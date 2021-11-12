6234386ede9c07c8c741c33e3901ab36a17383e6
Michael Piseno <mpiseno@gatech.edu>
2021-11-11 20:55:42 -0800

    added ability to save all data to a big numpy file

--- generate-SwAV-embeddings.py
+++ generate-SwAV-embeddings.py
@@ -13,6 +13,7 @@ from util import *
 MINI_IMAGENET_ROOT = './mini-imagenet/'
 LARGE_EMBEDDING_ROOT = './mini-imagenet-SwAV/'
 SMALL_EMBEDDING_ROOT = './mini-imagenet-SwAV-reduced/'
+NPY_ROOT = './reduced-npy/'
 
 # Must download mini-imagenet dataset first
 
@@ -66,7 +67,7 @@ def generate_large_embeddings():
         
         return len(dataset)
 
-    data_len = sum([embed(split_type) for split_type in ['test', 'val']])
+    data_len = sum([embed(split_type) for split_type in ['train', 'test', 'val']])
     print(f'Created {data_len} embeddings in {time.time() - start} seconds.')
 
 
@@ -80,16 +81,43 @@ def reduce_embeddings(n_components):
     start = time.time()
 
     def apply_PCA(split_type):
+        print(f'split: {split_type}')
         data = EmbeddingDataset(os.path.join(LARGE_EMBEDDING_ROOT, split_type))
         Z = torch.stack([d[0] for d in data]).detach().numpy()
         filepaths = [d[2] for d in data]
 
-        pca = PCA(n_components=n_components)
+        pca = PCA(n_components=n_components, whiten=True)
         reduced_Z = pca.fit_transform(Z)
         save_embeddings(reduced_Z, filepaths, LARGE_EMBEDDING_ROOT, SMALL_EMBEDDING_ROOT)
 
+        print(f'explained variance ratios: {pca.explained_variance_ratio_} | sum: {sum(pca.explained_variance_ratio_)}')
+
     _ = [apply_PCA(split_type) for split_type in ['train', 'val', 'test']]
     print(f'Reduced embeddings in {time.time() - start} seconds.')
+
+
+def generate_npy_datasets():
+    start = time.time()
+    def generate_dataset(split_type):
+        data = ImageDatasetWithFiles(os.path.join(MINI_IMAGENET_ROOT, split_type))
+        imgs = [d[0] for d in data]
+        filepaths = [d[2] for d in data]
+
+        # labels in other folders are meaningless! because of the way pytorch dataloader generates labels.
+        # It assumes all classes are represented in the dataset, but in miniimagenet, different classes are in
+        # each of test train and val.
+        labels = generate_labels(filepaths, split_type)
+        save_npy_arrays(
+            imgs,
+            labels,
+            filepaths,
+            MINI_IMAGENET_ROOT, SMALL_EMBEDDING_ROOT, NPY_ROOT,
+            split_type
+        )
+
+    _ = [generate_dataset(split_type) for split_type in ['train', 'val', 'test']]
+    print(f'Generated npy in {time.time() - start} seconds.')
+
 
 
 if __name__ == '__main__':
@@ -97,6 +125,7 @@ if __name__ == '__main__':
     parser.add_argument('--generate_large', dest='generate_large', action='store_const', const=True, default=False)
     parser.add_argument('--reduce', dest='reduce', action='store_const', const=True, default=False)
     parser.add_argument('--n_components', type=int, default=64)
+    parser.add_argument('--generate_npy', dest='generate_npy', action='store_const', const=True, default=False)
     args = parser.parse_args()
 
     generate_dirs(MINI_IMAGENET_ROOT, LARGE_EMBEDDING_ROOT)
@@ -109,3 +138,6 @@ if __name__ == '__main__':
     # Reduces the dim of embeddings from SwAV through PCA
     if args.reduce:
         reduce_embeddings(n_components=args.n_components)
+
+    if args.generate_npy:
+        generate_npy_datasets()
--- util.py
+++ util.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import torchvision.datasets as datasets
 from torchvision.datasets.folder import default_loader
@@ -48,3 +49,37 @@ def save_embeddings(embeddings, filepaths, old_root, new_root):
     for z, fp in zip(embeddings, filepaths):
         new_fp = fp.replace(old_root, new_root).replace('.jpg', '.pt')
         torch.save(z, new_fp)
+
+
+def generate_labels(filepaths, split_type):
+    split_to_range = {
+        'train': list(range(0, 64)),
+        'val': list(range(64, 80)),
+        'test': list(range(80, 100))
+    }
+    imagenet_labels = [fp.split('/')[-2] for fp in filepaths]
+    label_map = dict(
+        (label, split_to_range[split_type][i]) for i, label in enumerate(np.unique(imagenet_labels))
+    )
+    labels = [label_map[label] for label in imagenet_labels]
+    return labels
+
+
+def save_npy_arrays(imgs, labels, filepaths, img_root, embedding_root, npy_root, split_type):
+    X = [] # images
+    Y = [] # labels
+    Z = [] # data
+    for img, label, fp in zip(imgs, labels, filepaths):
+        X.append(np.array(img))
+        Y.append(label)
+        
+        
+        z_fp = fp.replace(img_root, embedding_root).replace('.jpg', '.pt')
+        z = torch.load(z_fp)
+        Z.append(z)
+
+    X = np.array(X)
+    Y = np.array(Y)
+    Z = np.array(Z)
+    save_path = f'./{npy_root}/{split_type}.npy'
+    np.save(save_path, {'X': X, 'Y': Y, 'Z': Z}, allow_pickle=True)
